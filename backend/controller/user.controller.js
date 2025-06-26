325dd0270c9f921a1fef2ae5b7a8865ea8ed5cea
Anushree <anushreemehta6@gmail.com>
2025-06-26 17:55:15 +0530

    login backend added

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -5,21 +5,21 @@ import bcrypt from 'bcryptjs';
 
 export const signup = async (req, res) => {
     try {
-        const { firstname, lastname, email, password } = req.body;  
-    const hashedPassword = await bcrypt.hash(password, 10);
-      
+        const { firstname, lastname, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             console.log("User already exists");
             return res.status(400).json({ message: "User already exists" });
         }
 
-        
+
         const user = new User({
             firstname,
             lastname,
             email,
-            password  : hashedPassword
+            password: hashedPassword
         });
 
         await user.save();
@@ -31,3 +31,33 @@ export const signup = async (req, res) => {
         res.status(500).json({ message: "Error occurred while creating user" });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+    const { email, password } = req.body;
+
+   
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+
+  
+    res.status(200).json({ message: "User logged in successfully!", user });
+
+  } catch (error) {
+        console.log("error", error)
+
+    }
+
+
+
+}
