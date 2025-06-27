60c84d32487d54140a35290239997b66adbff4ad
Anushree <anushreemehta6@gmail.com>
2025-06-27 14:29:22 +0530

    login backend interegrated with frontend

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 import Navbar from './home/Navbar'
 import Footer from './home/Footer'
 import { Link } from "react-router";
-
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import axios from 'axios';
 function Login() {
+    const [email, setEmail] = useState('');
+  
+  const [password, setPassword] = useState('');
+
+  const signup = async (e) => {
+    e.preventDefault(); 
+    try {
+      const res = await axios.post('http://localhost:4001/user/login', {
+       
+        email,
+        password
+      });
+      console.log(res.data);
+      toast.success('User logined successfully!');
+    } catch (error) {
+      toast.error('Signup failed!');
+      console.error("Signup error:", error);
+    }
+  };
+
   return (
     <>
     <Navbar/>
@@ -19,17 +41,17 @@ function Login() {
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form onSubmit={signup} class="space-y-4 md:space-y-6" action="#">
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                      <input type="email" onChange={(e)=>{setEmail(e.target.value)}} name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                      <input type="password" onChange={(e)=>{setPassword(e.target.value)}} name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                   </div>
                
-                  <button type="submit" class="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">Sign in</button>
+                  <button type="submit" class="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">Log in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400 flex gap-5 items-center justify-center">
                       Don’t have an account yet? <Link to={"/signup"}  class= " btn text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800" >Sign up</Link>
                   </p>
