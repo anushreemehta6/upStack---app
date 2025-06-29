cce5e78fd53b2213b3399bf59a436bd6065b5b38
Anushree <anushreemehta6@gmail.com>
2025-06-29 23:40:35 +0530

    config file updated

--- backend/config.js
+++ backend/config.js
@@ -10,4 +10,4 @@ const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD
 if (!JWT_ADMIN_PASSWORD) {
   console.warn("⚠️ JWT_USER_PASSWORD is not defined in .env");
 }
-export default {JWT_ADMIN_PASSWORD,JWT_ADMIN_PASSWORD}
+export default {JWT_USER_PASSWORD,JWT_ADMIN_PASSWORD}
